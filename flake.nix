b8420c36890befca6c372ccc1c573b1f1586ce48
Yongun Seong <nevivurn@nevi.dev>
2023-12-14 22:02:33 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          nativeBuildInputs = with pkgs; [ mysql ];
+        };
+        packages.default = pkgs.python3Packages.buildPythonApplication {
+          name = "db-proj2";
+          src = ./.;
+          format = "other";
+
+          nativeBuildInputs = with pkgs.python3Packages; [
+            mypy
+          ];
+
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            mysql-connector
+            numpy
+            pandas
+          ];
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm755 run.py $out/bin/run.py
+            runHook postInstall
+          '';
+
+          meta.mainProgram = "run.py";
+        };
+      }
+    );
+}
